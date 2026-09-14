a0f66c4bef84793acf6d606078a2f5045c0ececa
wardt <wardt@ARMOURY>
2026-09-13 20:49:54 -0400

    Square Spawner

---
+++ Ward_Tyler_Motion2026_ProperVersion/Assets/Scripts/SquareSpawner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SquareSpawner : MonoBehaviour
+{
+    public Transform aTransform; //x=-4 y=2
+    public Transform bTransform; //x=4 y=2
+    public Transform cTranform; //x=-4 y=-2
+    public Transform dTransform; //x=4 y=-2
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //transform.position = mousePos;
+
+        //if (transform.position = mousePos)
+        //{
+        //    Debug.DrawLine(aTransform.position, bTransform.position, Color.red);
+        //}
+
+        bool Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if (mousePos == true)
+        {
+            Debug.DrawLine(aTransform.position, bTransform.position, Color.magenta);
+        }
+
+    }
+}
