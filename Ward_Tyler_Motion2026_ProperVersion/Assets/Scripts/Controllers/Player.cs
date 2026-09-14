8e2185ed51d30bf3b41bf74dd8a9653c926ace7e
wardtyl <wardtyl@OA-S142-09>
2026-09-14 15:14:17 -0400

    check for changes

---
+++ Ward_Tyler_Motion2026_ProperVersion/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+
+    void Update()
+    {
+
+    }
+}
