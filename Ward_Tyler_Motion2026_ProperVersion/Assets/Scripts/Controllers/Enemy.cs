8720ab83198d87bfce5ab6c2e9f8718c2a0b22f1
wardtyl <wardtyl@OA-S318-12>
2026-09-25 13:42:12 -0400

    task 4b enemy motion implementation

--- Ward_Tyler_Motion2026_ProperVersion/Assets/Scripts/Controllers/Enemy.cs
+++ Ward_Tyler_Motion2026_ProperVersion/Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,42 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
-    private void Update()
+
+    public float maxSpeed;
+    public float accelerationTime;
+    public float decelerationTime;
+    public float currentAcceleration;
+    public float currentDeceleration;
+    public Vector3 currentVelocity = Vector3.right;
+
+    void Start()
     {
+        currentAcceleration = maxSpeed / accelerationTime;
+        currentDeceleration = maxSpeed / decelerationTime;
+    }
+
+    void Update()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector3 accelerationDirection = Vector3.zero;
+
+        if (screenPos.x < 0)
+        {
+            accelerationDirection += Vector3.right;
+        }
+        if (screenPos.x > Screen.width)
+        {
+            accelerationDirection += Vector3.left;
+        }
+
+        currentVelocity += accelerationDirection.normalized * currentAcceleration * Time.deltaTime;
+
+        //Deceleration
+        //if (accelerationDirection == Vector3.zero)
+        //{
+        //    currentVelocity += currentDeceleration * -currentVelocity.normalized * Time.deltaTime;
+        //}
     }
 
 }
